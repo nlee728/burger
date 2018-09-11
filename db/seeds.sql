f637a32edbd43a082b33f3ec01160aa4e20d64d1
unknown <38478896+nlee728@users.noreply.github.com>
2018-09-11 13:46:55 -0400

    starter files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+-- Insert a set of records.
+INSERT INTO burgers (burger) VALUES (The Captain America);
+INSERT INTO burgers (burger) VALUES (The Black Panther);
+INSERT INTO burgers (burger) VALUES (The Ant Man);
+INSERT INTO burgers (burger) VALUES (The Black Widow);
+INSERT INTO burgers (burger) VALUES (The Valkyrie);
